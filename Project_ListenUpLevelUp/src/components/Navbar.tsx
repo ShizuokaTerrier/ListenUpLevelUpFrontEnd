2b2ab4c28af621ff5393257360723a50a4849833
ShizuokaTerrier <matt@squareoneJapan.com>
2024-04-16 16:27:59 +0900

    initial commit

---
+++ Project_ListenUpLevelUp/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import '../App.css';
+
+function Navbar() {
+  return (
+    <div className='App'>
+      <nav className='navbar'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Register/Login</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
