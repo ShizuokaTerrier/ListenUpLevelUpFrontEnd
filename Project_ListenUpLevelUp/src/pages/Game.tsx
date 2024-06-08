60b64d1b491d8d07496e5f7576329d4fa2ebb31b
ShizuokaTerrier <matt@squareoneJapan.com>
2024-06-08 19:13:56 +0900

    feat: add basic styling for game page and create speech synthesis function

--- Project_ListenUpLevelUp/src/pages/Game.tsx
+++ Project_ListenUpLevelUp/src/pages/Game.tsx
@@ -1,5 +1,5 @@
 import { index } from '@material-tailwind/react/types/components/select';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function Game() {
   const arrayOfPairs = [
@@ -16,9 +16,23 @@ function Game() {
     answerFour: 0,
   });
 
+  const answerArray = [
+    answers.answerOne,
+    answers.answerTwo,
+    answers.answerThree,
+    answers.answerFour,
+  ];
+
   function flipACoin() {
     return Math.floor(Math.random() * 2);
   }
+
+  function playWord(word: string) {
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.rate = 1;
+    speechSynthesis.speak(utterance);
+  }
+
   useEffect(() => {
     setAnswers((prevState) => ({
       ...prevState,
@@ -29,31 +43,37 @@ function Game() {
     }));
   }, []);
 
-  const answerArray = [
-    answers.answerOne,
-    answers.answerTwo,
-    answers.answerThree,
-    answers.answerFour,
-  ];
-
   return (
     <>
-      <div>
-        <h1>Game Page</h1>
-      </div>
-      <tbody>
-        {arrayOfPairs.map((item: string[], index: index) => {
-          return (
-            <tr key={index}>
-              <div>
-                <button>Play</button>
-                <button>{item[0]}</button>
-                <button>{item[1]}</button>
-              </div>
-            </tr>
-          );
-        })}
-      </tbody>
+      <body className='min-h-screen bg-slate-50 flex justify-center items-center '>
+        <div className='bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4'>
+          <div>
+            <h3 className='p-5 text-center sm:text-left font-extrabold'>
+              Game Page
+            </h3>
+          </div>
+          <tbody className=''>
+            {arrayOfPairs.map((item: string[], index: index) => {
+              return (
+                <tr key={index}>
+                  <div className='flex justify-start'>
+                    <button
+                      className=' bg-blue-500 border rounded-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline'
+                      onClick={() => {
+                        playWord(item[answerArray[index]]);
+                      }}
+                    >
+                      Play
+                    </button>
+                    <button className=' p-2'>{item[0]}</button>
+                    <button className=' p-2'>{item[1]}</button>
+                  </div>
+                </tr>
+              );
+            })}
+          </tbody>
+        </div>
+      </body>
     </>
   );
 }
