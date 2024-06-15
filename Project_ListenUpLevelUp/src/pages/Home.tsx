676c5f6ca91ea9fbead770d4ee1d51ddec9043b8
ShizuokaTerrier <matt@squareoneJapan.com>
2024-06-15 17:42:16 +0900

    feat: create card menu for games to be made

--- Project_ListenUpLevelUp/src/pages/Home.tsx
+++ Project_ListenUpLevelUp/src/pages/Home.tsx
@@ -1,36 +1,159 @@
 function Home() {
   return (
-    <body className='min-h-screen bg-slate-50 flex justify-center items-start'>
+    <body className='min-h-screen bg-slate-50'>
       <div>
         <h1 className='text-center sm:text-left text-3xl font font-medium p-5'>
           Listen Up Level Up
         </h1>
-        <p className='p-5'>
+        <p className='p-5 text-center'>
           Listen Up Level Up is a web app on a mission to boost your English
-          listening skills by honing your ability to recognize and understand
+          listening skills <br /> Hone your ability to recognize and understand
           specific English sounds.
         </p>
-        <div className='max-w-sm rounded overflow-hidden shadow-lg lg:flex '>
-          <img src='' alt='' />
-          <div className='px-6 py -4 items-center'>
-            <h3 className=' font-bold text-xl mb-2 text-center'>
-              Minimal Pairs Games
-            </h3>
-            <p className=' text-gray-700 text-base text-center'>
-              Two words that differ by one sound only. <br /> Can you tell the
-              difference?
-            </p>
+      </div>
+      <div className='flex mb-4 justify-center flex-wrap'>
+        <div>
+          <div className=' max-w-sm rounded overflow-hidden shadow-lg m-2'>
+            <img src='' alt='' />
+            <div className='px-6 py-4'>
+              <h3 className=' font-bold text-xl mb-2 text-center'>
+                Minimal Pairs Games
+              </h3>
+              <p className=' text-gray-700 text-base text-center'>
+                Two words that differ by one sound only. <br /> Can you tell the
+                difference?
+              </p>
+            </div>
+            <div className='px-6 pt-4 pb-2 '>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Listening
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation Awareness
+              </span>
+            </div>
           </div>
-          <div className='px-6 pt-4 pb-2'>
-            <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-              #Listening
-            </span>
-            <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-              #Pronunciation
-            </span>
-            <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-              #Pronunciation Awareness
-            </span>
+        </div>
+        <div>
+          <div className=' max-w-sm rounded overflow-hidden shadow-lg m-2'>
+            <img src='' alt='' />
+            <div className='px-6 py-4'>
+              <h3 className=' font-bold text-xl mb-2 text-center'>
+                Dictation Machine
+              </h3>
+              <p className=' text-gray-700 text-base text-center'>
+                DIctation challenges. <br /> Can you catch what the computer is
+                saying?
+              </p>
+            </div>
+            <div className='px-6 pt-4 pb-2'>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Listening
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation Awareness
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className=' max-w-sm rounded overflow-hidden shadow-lg m-2'>
+            <img src='' alt='' />
+            <div className='px-6 py-4'>
+              <h3 className=' font-bold text-xl mb-2 text-center'>
+                Syllable Session
+              </h3>
+              <p className=' text-gray-700 text-base text-center'>
+                Surely syllables are simple? <br /> Count them and see!
+              </p>
+            </div>
+            <div className='px-6 pt-4 pb-2 '>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Listening
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation Awareness
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className=' max-w-sm rounded overflow-hidden shadow-lg m-2'>
+            <img src='' alt='' />
+            <div className='px-6 py-4'>
+              <h3 className=' font-bold text-xl mb-2 text-center'>IPA Drill</h3>
+              <p className=' text-gray-700 text-base text-center'>
+                Learn all the IPA symbols <br /> Just listen and click!
+              </p>
+            </div>
+            <div className='px-6 pt-4 pb-2 '>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Listening
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation Awareness
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className=' max-w-sm rounded overflow-hidden shadow-lg m-2'>
+            <img src='' alt='' />
+            <div className='px-6 py-4'>
+              <h3 className=' font-bold text-xl mb-2 text-center'>
+                Minimal Pairs Journey
+              </h3>
+              <p className=' text-gray-700 text-base text-center'>
+                Listen to the words. <br /> Follow them to the end of the road!
+              </p>
+            </div>
+            <div className='px-6 pt-4 pb-2 '>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Listening
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation Awareness
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className=' max-w-sm rounded overflow-hidden shadow-lg m-2'>
+            <img src='' alt='' />
+            <div className='px-6 py-4'>
+              <h3 className=' font-bold text-xl mb-2 text-center'>
+                IPA to English
+              </h3>
+              <p className=' text-gray-700 text-base text-center'>
+                Read the IPA <br /> Write the real spelling!
+              </p>
+            </div>
+            <div className='px-6 pt-4 pb-2 '>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Listening
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation
+              </span>
+              <span className=' inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+                #Pronunciation Awareness
+              </span>
+            </div>
           </div>
         </div>
       </div>
