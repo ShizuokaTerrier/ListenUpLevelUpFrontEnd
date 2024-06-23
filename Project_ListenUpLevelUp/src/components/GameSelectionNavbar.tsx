f12c7916944b159ffb4a3e47b2cb95b4c069a84a
ShizuokaTerrier <matt@squareoneJapan.com>
2024-06-23 19:53:58 +0900

    feat: basic outline of a game selection navbar to render inside each game on the game page. Not functional.

---
+++ Project_ListenUpLevelUp/src/components/GameSelectionNavbar.tsx
@@ -0,0 +1,18 @@
+const GameSelectionNavbar = () => {
+  return (
+    <div>
+      <nav className='p-5 bg-gray-200 shadow'>
+        <ul className='flex justify-between items-center'>
+          <li>Minimal Pairs</li>
+          <li>Dictation Machine</li>
+          <li>Syllable Session</li>
+          <li>IPA Drill</li>
+          <li>Minimal Pairs Journey</li>
+          <li>IPA to English</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default GameSelectionNavbar;
